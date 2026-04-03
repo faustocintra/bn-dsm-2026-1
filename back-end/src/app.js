e24727fa733bb9fe8dcefd407d0c134eb49d026b
Rafael Veronez <rafaelveronez@MacBook-Air-de-Rafael.local>
2026-04-03 15:23:56 -0300

    (23/03) Instalação e configuração do Prisma ORM && (30/03) Inicio da implementação do CRUD de categorias

---
+++ back-end/src/app.js
@@ -0,0 +1,23 @@
+import express, { json, urlencoded } from 'express'
+import cookieParser from 'cookie-parser'
+import logger from 'morgan'
+
+import indexRouter from './routes/index.js'
+import usersRouter from './routes/users.js'
+
+const app = express()
+
+app.use(logger('dev'))
+app.use(json())
+app.use(urlencoded({ extended: false }))
+app.use(cookieParser())
+
+
+
+/******************** ROTAS ********************/
+import categoriasRouter from './routes/categorias.js'
+app.use('/categorias', categoriasRouter)
+app.use('/', indexRouter)
+app.use('/users', usersRouter)
+
+export default app
---
+++ back-end/src/routes/categorias.js
@@ -0,0 +1,11 @@
+import { Router } from 'express'
+import controller from '../controllers/categoria.js'
+
+
+const router = Router()
+
+
+router.post('/', controller.create)
+
+
+export default router
