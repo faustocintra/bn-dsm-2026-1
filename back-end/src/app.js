ef8b8d4da8ee0fe9d3c4f15f8c7b6979f87b7055
Marcelo Morais Bueno <marcelomoraisbueno@gmail.com>
2026-04-28 00:40:15 +0000

    (27/04) Código completo do back-end, com correções

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -18,6 +18,18 @@ app.use('/users', usersRouter)
 /******************** ROTAS ********************/
 
 import categoriasRouter from './routes/categorias.js'
-app.use('/categorias', categoriasRouter) 
+app.use('/categorias', categoriasRouter)
+
+import clientesRouter from './routes/clientes.js'
+app.use('/clientes', clientesRouter)
+
+import fornecedoresRouter from './routes/fornecedores.js'
+app.use('/fornecedores', fornecedoresRouter)
+
+import pedidosRouter from './routes/pedidos.js'
+app.use('/pedidos', pedidosRouter)
+
+import produtosRouter from './routes/produtos.js'
+app.use('/produtos', produtosRouter)
 
 export default app
