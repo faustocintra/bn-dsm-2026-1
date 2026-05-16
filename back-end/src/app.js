f328cc923763b0da9346ad46054b5fce316e31c4
Rafael Veronez <rafaelveronez@MacBook-Air-de-Rafael.local>
2026-05-16 17:51:58 -0300

    (27/04) codigo completo backend, com correções

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -12,12 +12,24 @@ app.use(json())
 app.use(urlencoded({ extended: false }))
 app.use(cookieParser())
 
-
-
-/******************** ROTAS ********************/
-import categoriasRouter from './routes/categorias.js'
-app.use('/categorias', categoriasRouter)
 app.use('/', indexRouter)
 app.use('/users', usersRouter)
+
+/******************** ROTAS ********************/
+
+import categoriasRouter from './routes/categorias.js'
+app.use('/categorias', categoriasRouter)
+
+import clientesRouter from './routes/clientes.js'
+app.use('/clientes', clientesRouter)
+
+import fornecedoresRouter from './routes/fornecedores.js'
+app.use('/fornecedores', fornecedoresRouter)
+
+import pedidosRouter from './routes/pedidos.js'
+app.use('/pedidos', pedidosRouter)
+
+import produtosRouter from './routes/produtos.js'
+app.use('/produtos', produtosRouter)
 
 export default app
